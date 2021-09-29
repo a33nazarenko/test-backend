d7dedfb1297fd67271be72f08d52d5917ea3fff9
Dominus666 <38140505+Dominus666@users.noreply.github.com>
2021-09-29 11:45:35 +0300

    added prettier

--- src/services/AuthService/auth.service.ts
+++ src/services/AuthService/auth.service.ts
@@ -1,9 +1,8 @@
 import { Request } from 'express';
 import { UserModel } from '../../models/user/user.model';
-import {client} from '../../app'
+import { client } from '../../app';
 
 class AuthService {
-
   public login = async () => {
     const newUser = new UserModel();
     const response = await newUser.save();
@@ -15,29 +14,24 @@ class AuthService {
   };
 
   public sendCode = async (phoneNumber: string, channel: string) => {
-
-        const result = await client
-              .verify
-              .services(`${process.env.TWILIO_VERIFICATION_SID}`)
-              .verifications
-              .create({
-                  to: `+${phoneNumber}`,
-                  channel: channel
-              });
-              return result;
-};
-
-public verifyCode = async (phoneNumber: string, code: string) => {
-  const result = await client
-        .verify
-        .services(`${process.env.TWILIO_VERIFICATION_SID}`)
-        .verificationChecks
-        .create({
-            to: `+${phoneNumber}`,
-            code: code
-        })
-        return result;
+    const result = await client.verify
+      .services(`${process.env.TWILIO_VERIFICATION_SID}`)
+      .verifications.create({
+        to: `+${phoneNumber}`,
+        channel,
+      });
+    return result;
   };
-};
+
+  public verifyCode = async (phoneNumber: string, code: string) => {
+    const result = await client.verify
+      .services(`${process.env.TWILIO_VERIFICATION_SID}`)
+      .verificationChecks.create({
+        to: `+${phoneNumber}`,
+        code,
+      });
+    return result;
+  };
+}
 
 export const AuthServiceAPI = new AuthService();
